3cc8138c3abe3d6d57b76d1ecbc36ba2ad28e0a6
RafaelGaiteiro <rafaelkramerdasilva@gmail.com>
2023-06-01 13:44:05 -0300

    update deatecs

--- src/repositores/listOfPeople.ts
+++ src/repositores/listOfPeople.ts
@@ -1,54 +1,28 @@
 export default [
-  // { id: 1, 
-  //   name: "Rafael", 
-  //   company: "M8 Sistemas", 
-  //   git: "RafaelGaiteiro" 
+  // { id: 1,
+  //   name: "Rafael",
+  //   company: "M8 Sistemas",
+  //   git: "RafaelGaiteiro"
   // },
-  { id: 2, 
-    name: "Thais", 
-    company: "M8 Sistemas", 
-    git: "thaisrtz" 
-  },
-  { id: 3, 
-    name: "Alexandre", 
-    company: "Infogen", 
-    git: "AlexandreLanga" 
-  },
+  { id: 2, name: "Thais", company: "M8 Sistemas", git: "thaisrtz" },
+  // { id: 3, name: "Alexandre", company: "Infogen", git: "AlexandreLanga" },
   {
     id: 4,
     name: "Aquiles",
     company: "Multisoftware",
     git: "AquilesMoroni",
   },
-  {
-    id: 5,
-    name: "Valtemir",
-    company: "Multisoftware",
-    git: "ValtemirJr",
-  },
-  { id: 6, 
-    name: "Gabriel", 
-    company: "IXCsoft", 
-    git: "gabreaw" 
-  },
-  { id: 7, 
-    name: "Vanessa", 
-    company: "Amo", 
-    git: "nessavs" 
-  },
-  { id: 8, 
-    name: "Angelo", 
-    company: "Amo", 
-    git: "angelosfreddo" 
-  },
-  { id: 9, 
-    name: "João", 
-    company: "VisionSysten", 
-    git: "" 
-  },
-  { id: 10, 
-    name: "Rian", 
-    company: "Gamatec", 
-    git: "" 
-  },
+  // {
+  //   id: 5,
+  //   name: "Valtemir",
+  //   company: "Multisoftware",
+  //   git: "ValtemirJr",
+  // },
+  { id: 6, name: "Gabriel", company: "IXCsoft", git: "gabreaw" },
+  { id: 7, name: "Vanessa", company: "Amo", git: "nessavs" },
+  { id: 8, name: "Angelo", company: "Amo", git: "angelosfreddo" },
+  { id: 9, name: "João", company: "VisionSystem", git: "joaaoinacio" },
+  { id: 10, name: "Rian", company: "Gamatec", git: "" },
+  { id: 11, name: "Gabriel K.", company: "Trafegus", git: "Gabigks" },
+  { id: 12, name: "Cassiane Cover", company: "Deatec", git: "deatechsoft" },
 ];
